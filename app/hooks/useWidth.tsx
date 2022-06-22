0ed57f67a89b2a9ad52ed04b9280655ac7ad3dc7
Jake <jakegod@live.ca>
2022-06-22 19:56:05 -0400

    Add useWidth hook

---
+++ app/hooks/useWidth.tsx
@@ -0,0 +1,17 @@
+import type { Breakpoint, Theme } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+type BreakpointOrNull = Breakpoint | null;
+
+export function useWidth() {
+  const theme: Theme = useTheme();
+  const keys: readonly Breakpoint[] = [...theme.breakpoints.keys].reverse();
+  return (
+    keys.reduce((output: BreakpointOrNull, key: Breakpoint) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const matches = useMediaQuery(theme.breakpoints.up(key));
+      return !output && matches ? key : output;
+    }, null) || 'xs'
+  );
+}
